39bc38e3b3e43078e38bc15280ac46c67e7de564
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-02-08 16:43:13 -0300

    Criar o Context da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-function App() {
-  return <h1>App</h1>;
-}
+import { CotizadorProvider } from "./context/CotizadorProvider";
+import AppSeguro from "./components/AppSeguro";
+
+const App = () => {
+  return (
+    <CotizadorProvider>
+      <AppSeguro />
+    </CotizadorProvider>
+  );
+};
 
 export default App;
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,67 @@
+import { Fragment } from "react";
+
+import { MARCAS, YEARS, PLANES } from "../constants";
+
+const Formulario = () => {
+  return (
+    <Fragment>
+      <form>
+        <div className="my-5">
+          <label className="block mb-3 font-bold text-gray-400 uppercase">
+            Marca:
+          </label>
+          <select
+            name="marca"
+            className="w-full p-3 bg-white border border-gray-200"
+          >
+            <option value="">-- Selecciona Marca --</option>
+            {MARCAS.map((marca) => (
+              <option key={marca.id} value={marca.id}>
+                {marca.nombre}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="my-5">
+          <label className="block mb-3 font-bold text-gray-400 uppercase">
+            Ano:
+          </label>
+          <select
+            name="marca"
+            className="w-full p-3 bg-white border border-gray-200"
+          >
+            <option value="">-- Selecciona Ano --</option>
+            {YEARS.map((year) => (
+              <option key={year} value={year}>
+                {year}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="my-5">
+          <label className="block mb-3 font-bold text-gray-400 uppercase">
+            Plano:
+          </label>
+          <div className="flex gap-3 items-center">
+            {PLANES.map((plan) => (
+              <Fragment key={plan.id}>
+                <label>{plan.nombre}</label>
+                <input type="radio" name="plan" value={plan.id} />
+              </Fragment>
+            ))}
+          </div>
+        </div>
+
+        <input
+          type="submit"
+          className="w-full bg-purple-900 hover:bg-indigo-800 transition-colors text-white cursor-pointer p-3 uppercase font-bold"
+          value="Cotizar"
+        ></input>
+      </form>
+    </Fragment>
+  );
+};
+
+export default Formulario;
