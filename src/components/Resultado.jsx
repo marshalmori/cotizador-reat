aa297360405d61ac3cedb09237d381bea8067337
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-02-14 21:20:54 -0300

    Criar o componente Resultado

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,11 +1,23 @@
 import useCotizador from "../hooks/useCotizador";
+import { MARCAS, PLANES } from "../constants";
 
 const Resultado = () => {
-  const { resultado } = useCotizador();
+  const { resultado, datos } = useCotizador();
+  const { marca, plan, year } = datos;
+
+  const [nombreMarca] = MARCAS.filter((m) => m.id === Number(marca));
 
   if (resultado === 0) return null;
 
-  return <div>{resultado}</div>;
+  return (
+    <div className="bg-gray-200 text-center mt-5 p-5 shadow">
+      <h2 className="text-gray-600 font-black text-2xl">Resumen</h2>
+      <p className="my-2">
+        <span className="font-bold">Marca: </span>
+        {nombreMarca.nombre}
+      </p>
+    </div>
+  );
 };
 
 export default Resultado;
